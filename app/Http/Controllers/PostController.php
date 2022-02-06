0be0a2ec4389bca7ceba713353dbc72bc99dcf85
dimad3 <dmitrijs633@gmail.com>
2022-02-06 12:32:29 +0200

    L18 - Post's Page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,16 +7,48 @@ use App\Post;
 
 class PostController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         //$posts = Post::with('category');
         $posts = Post::with('category')->orderBy('created_at', 'desc')->paginate(3);
         //dd (compact('posts'));
         return view('posts.index', compact('posts'));
     }
 
-    public function show($slug){
-        $post = Post::find($slug);
-        dd($post);
-        return view('posts.show', compact($post));
+    public function show($slug)
+    {
+        /**
+         * vendor\laravel\framework\src\Illuminate\Database\Eloquent\Builder.php
+         *
+         * public function findOrFail($id, $columns = ['*'])
+         *
+         * Find a model by its primary key or throw an exception.
+         *
+         * @param  mixed  $id
+         * @param  array  $columns
+         * @return \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Collection|static|static[]
+         *
+         * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+         */
+
+        /**
+         * vendor\laravel\framework\src\Illuminate\Database\Eloquent\Builder.php
+         *
+         * public function firstOrFail($columns = ['*'])
+         *
+         * Execute the query and get the first result or throw an exception.
+         *
+         * @param  array  $columns
+         * @return \Illuminate\Database\Eloquent\Model|static
+         *
+         * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+         */
+
+        $post = Post::where('slug', $slug)->firstOrFail();
+        // dump($post);
+        // dd(compact('post'));
+        $post->views +=1;
+        $post->update();
+        return view('posts.show', compact('post'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PostController@index')->name('home');
 Route::get('/article/{slug}', 'PostController@show')->name('posts.single');
+Route::get('/category/{slug}', 'CategoryController@show')->name('categories.single');
+Route::get('/tag/{slug}', 'TagController@show')->name('tags.single');
 
 Route::prefix('admin')->namespace('Admin')->middleware('admin')->group(function () {
     // Matches The "/admin/users" URL
